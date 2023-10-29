d5ec37fc5f032d058283c182766e70b0ecfeb994
VaheHovakimyan <88284245+VaheHovakimyan@users.noreply.github.com>
2023-10-29 16:08:36 +0400

    add some functions

--- menu.js
+++ menu.js
@@ -2,16 +2,21 @@ import menu from './menu.json' assert {type: 'json'};
 
 
 let menu_container = document.getElementById("menu_elements_container");
+let back_btn_div = document.getElementById("back_btn_div");
+let sidebar_div = document.getElementById("sidebar_div");
+let header_title = document.getElementById("header_title");
+let header_logo_search = document.getElementById("header_logo_search");
 
 
 
-      // Function to recursively draw JSON
-      function drawJSON(data, container) {
+
+    // Function to recursively draw JSON
+    function drawJSON(data, container) {
 
         const ul = document.createElement("ul");
+        ul.className = "ul";
         let liTitle = document.createElement("h1");
-
-        // console.log(data.group);
+        liTitle.className = "sidebar_group_title";
 
         if (data.group !== undefined) {
 
@@ -23,41 +28,122 @@ let menu_container = document.getElementById("menu_elements_container");
         let i = 0;
 
         for (const key in data) {
-          const li = document.createElement("li");
+          let li = document.createElement("li");
+          li.className = "sidebar_pages_title";
+          let arrow_icon = document.createElement("img");
+          arrow_icon.src = "./icons/main_list/arrow_icon.svg";
+          arrow_icon.className = "arrow_icon";
+
+        //   let page_icon = document.createElement("div");
+        //   page_icon.style.width = "100px";
+        //   page_icon.style.height = "100px";
+        //   page_icon.style.background = "red";
+
+        let page_icon = document.createElement("img");
+        page_icon.src = "./icons/sidebar/page_icon.svg";
+        page_icon.className = "sidebar_page_icon";
+
+        let page_icon_div = document.createElement("div");
+        page_icon_div.className = "sidebar_page_div_name";
+       
+        let hideMenuBoolean = true;
+
+        function HideMenuTitles(){
+
+          if(hideMenuBoolean){
+            li.textContent = ``;
+            arrow_icon.className = "hide_arrow_icon";
+            sidebar_div.style.width = "auto";
+            header_title.style.display = "none";
+            header_logo_search.style.width = "150px";
+            back_btn_div.style.transform = "rotate(180deg)";
+            hideMenuBoolean = false;
+          }else{
+            li.textContent = data.title;
+            arrow_icon.className = "arrow_icon";
+            sidebar_div.style.width = "400px";
+            header_title.style.display = "block";
+            header_logo_search.style.width = "400px";
+            back_btn_div.style.transform = "rotate(0deg)";
+            hideMenuBoolean = true;
+          }
+
           
+        }
+
+        back_btn_div.addEventListener("click", HideMenuTitles);
+          
+
 
           if (typeof data[key] === "object") {
 
-            li.appendChild(drawJSON(data[key], container));
-            ul.appendChild(li);
+            page_icon_div.appendChild(drawJSON(data[key], container));
+            ul.appendChild(page_icon_div);
             
           } else if(data.title !== undefined) {
 
             if (i === 0) {
-              li.textContent += `${data.title}`;
+
+              li.innerText += `${data.title}`;
+              if (data.title === "Categories" || data.title === "Posts") {
+                page_icon_div.style.background = "#3A57E8";
+              }
+              page_icon_div.appendChild(page_icon);
+              page_icon_div.appendChild(li);
+
+              // let hideMenuBoolean = true;
+
+              // function HideMenuTitles(){
+
+              //   if(hideMenuBoolean){
+              //     li.textContent = ``;
+              //     page_icon_div.removeChild(arrow_icon);
+              //     sidebar_div.style.width = "auto";
+              //     header_title.style.display = "none";
+              //     header_logo_search.style.width = "150px";
+              //     back_btn_div.style.transform = "rotate(180deg)";
+              //     hideMenuBoolean = false;
+              //   }else{
+              //     li.textContent = `${data.title}`;
+              //     page_icon_div.appendChild(li);
+              //     page_icon_div.appendChild(arrow_icon);
+              //     sidebar_div.style.width = "400px";
+              //     header_title.style.display = "block";
+              //     header_logo_search.style.width = "400px";
+              //     back_btn_div.style.transform = "rotate(0deg)";
+              //     hideMenuBoolean = true;
+              //   }
+
+                
+              // }
+
+              // back_btn_div.addEventListener("click", HideMenuTitles);
+
+              if (data.children.length !== 0) {
+
+                page_icon_div.appendChild(arrow_icon);
+               
+              }
+
               i++;
-              ul.appendChild(li);
+              ul.appendChild(page_icon_div);
             }
             
-          }
+        }
+
          
 
           // ul.appendChild(li);
       
         }
 
+        
+
         return ul;
       }
 
 
       menu_container.appendChild(drawJSON(menu, menu_container));
-
-
-
-
-
-
-
 
 
 
